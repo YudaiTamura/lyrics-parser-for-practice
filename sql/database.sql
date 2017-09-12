c3d74421ed920fbb5758ce45b3e4623f93387b05
田村 侑大 <Yudai@MBP-09.local>
2017-09-12 15:01:06 +0900

    １から正規化をしなおした

--- sql/database.sql
+++ sql/database.sql
@@ -2,35 +2,54 @@ CREATE DATABASE lyrics_database;
 USE lyrics_database;
 
 
-
-CREATE TABLE IF NOT EXISTS `lyric` (
+CREATE TABLE IF NOT EXISTS `song` (
   `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `lyric` TEXT,
   `title` VARCHAR(50),
-  PRIMARY KEY(`id`)
-  UNIQUE(`lyric`) )
+  `singer_id` VARCHAR(50),
+  `lyric` TEXT,
+  PRIMARY KEY (`id`),
+  UNIQUE(`title`, `singer_id`)
+)
 DEFAULT CHARACTER SET = utf8;
 
 
-CREATE TABLE IF NOT EXISTS `lyric_singer` (
-  `lyric_id` INT(10) UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `singer` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `singer` VARCHAR(50),
-  UNIQUE(`lyric_id, singer`) )
+  `song_id` INT(10),
+  PRIMARY KEY (`id`) ),
+  UNIQUE(`singer`, `song_id`)
+)
 DEFAULT CHARACTER SET = utf8;
 
 
-CREATE TABLE IF NOT EXISTS `mp3_lyric` (
-  `mp3` VARCHAR(100),
-  `lyric_id` INT(10) UNSIGNED NOT NULL,
-  UNIQUE(`lyric_id, mp3`) )
+CREATE TABLE IF NOT EXISTS `version` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `version` VARCHAR(50),
+  PRIMARY KEY (`id`) ),
+  UNIQUE(`version`)
+)
 DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS `version_song` (
+  `song_id` INT(10),
+  `version` VARCHAR(50),
+  UNIQUE(`song_id`, `version`)
+)
+DEFAULT CHARACTER SET = utf8;
+
+
+
+
+
 
 
 CREATE TABLE IF NOT EXISTS `waiting` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `singer` VARCHAR(50),
   `title` VARCHAR(50),
-  PRIMARY KEY(`id`),
-  UNIQUE(`singer`, `title`) )
+  UNIQUE(`singer`, `title`)
+)
 DEFAULT CHARACTER SET = utf8;
+
 
