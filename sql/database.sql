6d9e832cc86715b8379fae0154c6d4c126590505
田村 侑大 <Yudai@MBP-09.local>
2017-09-19 18:01:14 +0900

    不具合修正

--- sql/database.sql
+++ sql/database.sql
@@ -1,8 +1,13 @@
-CREATE DATABASE lyrics_database;
-USE lyrics_database;
-
-
 CREATE TABLE IF NOT EXISTS `singer` (
+  `id`   INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50),
+  PRIMARY KEY (`id`)
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS `composer` (
   `id`   INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(50),
   PRIMARY KEY (`id`)
@@ -18,7 +23,7 @@ CREATE TABLE IF NOT EXISTS `song` (
   `lyric`       TEXT DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE(`title`, `composer_id`)
-  CONSTRAINT `fk_singer` FOREIGN KEY (`composer_id`) REFERENCES `composer` (`id`)
+  CONSTRAINT `fk_composer` FOREIGN KEY (`composer_id`) REFERENCES `composer` (`id`)
   ON DELETE NO ACTION ON UPDATE CASCADE,
 )
 ENGINE = InnoDB
@@ -28,7 +33,6 @@ DEFAULT CHARACTER SET = utf8;
 CREATE TABLE IF NOT EXISTS `singer_song` (
   `singer_id` INT(10) UNSIGNED NOT NULL,
   `song_id`   INT(10) UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`),
   UNIQUE(`singer_id`,`song_id`),
   CONSTRAINT `fk_singer` FOREIGN KEY (`singer_id`) REFERENCES `singer` (`id`)
   ON DELETE NO ACTION ON UPDATE CASCADE,
