97c2a3a4647d90965a9d5bae4664fa3b53ce44a2
田村 侑大 <Yudai@MBP-09.local>
2017-09-19 16:26:16 +0900

    構文ミスの修正・singer_songテーブルの追加・制約の追加

--- sql/database.sql
+++ sql/database.sql
@@ -2,37 +2,50 @@ CREATE DATABASE lyrics_database;
 USE lyrics_database;
 
 
-CREATE TABLE IF NOT EXISTS `song` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(50),
-  `singer_id` VARCHAR(50),
-  `lyric` TEXT,
-  PRIMARY KEY (`id`),
-  UNIQUE(`title`, `singer_id`)
-)
-DEFAULT CHARACTER SET = utf8;
-
-
-
 CREATE TABLE IF NOT EXISTS `singer` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `singer` VARCHAR(50),
-  `song_id` INT(10),
-  PRIMARY KEY (`id`) ),
-  UNIQUE(`singer`, `song_id`)
+  `id`   INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50),
+  PRIMARY KEY (`id`)
 )
+ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
+
+CREATE TABLE IF NOT EXISTS `song` (
+  `id`          INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title`       VARCHAR(50),
+  `composer_id` INT(10),
+  `lyric`       TEXT DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE(`title`, `composer_id`)
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS `singer_song` (
+  `id`        INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `singer_id` INT(10) UNSIGNED NOT NULL,
+  `song_id`   INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE(`singer_id`,`song_id`),
+  CONSTRAINT `fk_singer` FOREIGN KEY (`singer_id`) REFERENCES `singer` (`id`)
+  ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `fk_song` FOREIGN KEY (`song_id`) REFERENCES `song` (`id`)
+  ON DELETE NO ACTION ON UPDATE CASCADE
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 CREATE TABLE IF NOT EXISTS `version` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id`      INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `version` VARCHAR(50),
-  PRIMARY KEY (`id`) ),
+  PRIMARY KEY (`id`),
   UNIQUE(`version`)
 )
+ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
-
 
 
 CREATE TABLE IF NOT EXISTS `version_song` (
@@ -40,16 +53,6 @@ CREATE TABLE IF NOT EXISTS `version_song` (
   `version` VARCHAR(50),
   UNIQUE(`song_id`, `version`)
 )
+ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-
-
-
-CREATE TABLE IF NOT EXISTS `waiting` (
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `singer` VARCHAR(50),
-  `title` VARCHAR(50),
-  PRIMARY KEY (`id`) ),
-  UNIQUE(`singer`, `title`)
-)
-DEFAULT CHARACTER SET = utf8;
