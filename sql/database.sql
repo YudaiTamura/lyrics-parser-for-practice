8f42078a4c2a000a790dd2b65e3ffca0e2ce5fac
田村 侑大 <Yudai@MBP-09.local>
2017-11-08 15:56:47 +0900

    composerテーブルやversionテーブルを消してmp3pathカラムを追加することで、テーブル定義をアプリに沿ったものに変更

--- sql/database.sql
+++ sql/database.sql
@@ -7,33 +7,23 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
 
-CREATE TABLE IF NOT EXISTS `composer` (
-  `id`   INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(50),
+
+CREATE TABLE IF NOT EXISTS `song` (
+  `id`          INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title`       VARCHAR(50),
+  `lyric`       TEXT DEFAULT NULL,
   PRIMARY KEY (`id`)
 )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
 
-CREATE TABLE IF NOT EXISTS `song` (
-  `id`          INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `title`       VARCHAR(50),
-  `composer_id` INT(10) UNSIGNED NOT NULL,
-  `lyric`       TEXT DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE(`title`, `composer_id`),
-  CONSTRAINT `fk_composer` FOREIGN KEY (`composer_id`) REFERENCES `composer` (`id`)
-  ON DELETE NO ACTION ON UPDATE CASCADE
-)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
 
 CREATE TABLE IF NOT EXISTS `singer_song` (
   `singer_id` INT(10) UNSIGNED NOT NULL,
   `song_id`   INT(10) UNSIGNED NOT NULL,
-  UNIQUE(`singer_id`,`song_id`),
+  `mp3path`   TEXT DEFAULT NULL,
+  UNIQUE(`singer_id`,`song_id`, `mp3path`),
   CONSTRAINT `fk_singer` FOREIGN KEY (`singer_id`) REFERENCES `singer` (`id`)
   ON DELETE NO ACTION ON UPDATE CASCADE,
   CONSTRAINT `fk_song` FOREIGN KEY (`song_id`) REFERENCES `song` (`id`)
@@ -41,23 +31,3 @@ CREATE TABLE IF NOT EXISTS `singer_song` (
 )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
-
-
-CREATE TABLE IF NOT EXISTS `version` (
-  `id`      INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `version` VARCHAR(50),
-  PRIMARY KEY (`id`),
-  UNIQUE(`version`)
-)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-
-CREATE TABLE IF NOT EXISTS `version_song` (
-  `song_id` INT(10),
-  `version` VARCHAR(50),
-  UNIQUE(`song_id`, `version`)
-)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
